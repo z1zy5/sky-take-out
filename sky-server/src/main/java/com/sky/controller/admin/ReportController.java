557bda85fc0088e9de407c696f9636437de5d928
vv <3063495761@qq.com>
2024-07-29 09:45:26 +0800

    运营数据导出

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 import java.util.Date;
 
@@ -84,6 +85,12 @@ public class ReportController {
     ){
 
         return Result.success(reportService.getTop10(begin,end));
+    }
+
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据报表")
+    public void export(HttpServletResponse response){
+        reportService.exportBusinessData(response);
     }
 
 }
--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -5,6 +5,7 @@ import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 public interface ReportService {
@@ -28,8 +29,11 @@ public interface ReportService {
      * @param begin
      * @param end
      * @return
+     *
      */
     OrderReportVO getOrdersStatistics(LocalDate begin, LocalDate end);
 
     SalesTop10ReportVO getTop10(LocalDate begin, LocalDate end);
+
+    void exportBusinessData(HttpServletResponse response);
 }
